5911499a65e211b690ca9bc9f67a07b87fb4bbbf
SoulFireSagas <soulfiresagas@gmail.com>
2025-08-15 14:45:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "http://fond-marnia-soulfiresagas-e0ac340f.koyeb.app/")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
@@ -50,4 +50,5 @@ LOGGER_CONFIG_JSON = {
         }
     }
 }
+
 
