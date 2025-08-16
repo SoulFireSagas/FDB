da9866b82fdb2cb2d7accf794a4dd0c57a04b2ca
SoulFireSagas <soulfiresagas@gmail.com>
2025-08-16 17:49:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,19 +5,12 @@ from os import environ as env
 class Telegram:
 
     API_ID = int(env.get("TELEGRAM_API_ID", 22928570))
-
     API_HASH = env.get("TELEGRAM_API_HASH", "60bb37bddecb48c27c3e86906a077603")
-
     OWNER_ID = int(env.get("OWNER_ID", 2010016480))
-
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "2010016480").split()
-
     BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "FPDL_1Robot")
-
     BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "8450788394:AAGPcBSKFLLBnD4ED_Lxk2piHcJ-6iF5dgg")
-
     CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002744991028))
-
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 
@@ -25,19 +18,17 @@ class Telegram:
 class Server:
 
     BASE_URL = env.get("BASE_URL", "https://fond-marnia-soulfiresagas-e0ac340f.koyeb.app")
-
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-
     PORT = int(env.get("PORT", 8080))
 
 
 
     USE_BLOGGER_REDIRECT = True  # Set False to disable
     # Corrected: This should be a list of the actual Blogger redirect URLs.
-    BLOGGER_URLS = env.get("BLOGGER_URLS", "https://redirectarc.blogspot.com/2025/08/redirect.html").split(',')
+    BLOGGER_URL = env.get("BLOGGER_URL", "https://redirectarc.blogspot.com/2025/08/redirect.html").split(',')
     # This is the final download link that the Blogger page will use
     # It should be your server's /dl/ endpoint
-    RD_URL = env.get("RD_URL", f"{BASE_URL}/dl")
+    RD_URL = env.get("RD_URL", f"{BASE_URL}")
 
 
 
@@ -110,6 +101,7 @@ LOGGER_CONFIG_JSON = {
     }
 
 }
+
 
 
 
