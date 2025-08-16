fbfa3c712640b6c6fa24bdcca4345a73ed8b13ed
SoulFireSagas <soulfiresagas@gmail.com>
2025-08-16 16:15:48 +0530

    Update main.py

--- bot/server/main.py
+++ bot/server/main.py
@@ -13,8 +13,13 @@ async def home():
     return 'api is working'
 
 @bp.route('/RD')
-async def redirect():
-    return redirect(Server.RD_URL)
+async def handle_redirect():
+    file_id = request.args.get('file_id')
+    code = request.args.get('code')
+    
+    # Make sure Server.RD_URL is a proper string
+    redirect_url = f"{Server.RD_URL}?file_id={file_id}&code={code}"
+    return redirect(redirect_url)
     
 @bp.route('/dl/<int:file_id>')       
 async def transmit_file(file_id):
@@ -90,6 +95,7 @@ async def file_deeplink(file_id):
     code = request.args.get('code') or abort(401)
 
     return redirect(f'https://t.me/{Telegram.BOT_USERNAME}?start=file_{file_id}_{code}')
+
 
 
 
