b149d0c8cec6bdc9c01ed36e37b46b883076e91d
SoulFireSagas <soulfiresagas@gmail.com>
2025-08-17 14:13:38 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,4 @@
+# bot/__init__.py
 from telethon import TelegramClient
 from logging import getLogger
 from logging.config import dictConfig
@@ -8,7 +9,19 @@ dictConfig(LOGGER_CONFIG_JSON)
 version = 1.5
 logger = getLogger('bot')
 
-TelegramBot = TelegramClient(
+# Define the custom TelegramBot class with the bulk_cache attribute
+class TelegramBot(TelegramClient):
+    """
+    A custom class for our Telegram bot client.
+    This is where we'll store shared resources like the bulk file cache.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # This is the new dictionary to temporarily store bulk upload data
+        self.bulk_cache = {}
+
+# Now, create an instance of our custom TelegramBot class
+TelegramBot = TelegramBot(
     session='bot',
     api_id=Telegram.API_ID,
     api_hash=Telegram.API_HASH
