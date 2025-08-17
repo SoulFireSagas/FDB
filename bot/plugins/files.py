5e3037a5449377bf5a2013d2cbe1695af5087c56
SoulFireSagas <soulfiresagas@gmail.com>
2025-08-17 14:09:58 +0530

    Update files.py

--- bot/plugins/files.py
+++ bot/plugins/files.py
@@ -1,3 +1,5 @@
+# bot/plugins/files.py
+
 from telethon import Button
 from telethon.events import NewMessage
 from telethon.errors import MessageAuthorRequiredError, MessageNotModifiedError, MessageIdInvalidError
@@ -11,7 +13,8 @@ from bot.modules.static import *
 from urllib.parse import quote
 from bot.modules.telegram import get_file_properties
 
-
+# Temporary in-memory storage for bulk uploads.
+# Format: {user_id: {'name': '...','text': '...', 'files': [{'id': ..., 'size': ...}, ...]}}
 bulk_uploads = {}
 
 @TelegramBot.on(NewMessage(incoming=True, pattern='/bulk'))
@@ -100,52 +103,7 @@ async def end_bulk_upload(event: NewMessage.Event | Message):
         "This link will expire after a while."
     )
 
-@TelegramBot.on(NewMessage(incoming=True, func=filter_files))
-@verify_user(private=True)
-async def user_file_handler(event: NewMessage.Event | Message):
-    secret_code = token_hex(Telegram.SECRET_CODE_LENGTH)
-    event.message.text = f'`{secret_code}`'
-    message = await send_message(event.message)
-    message_id = message.id
-
-    
-
-    dl_link = f'{Server.BASE_URL}/RD/{message_id}?code={secret_code}'
-        
-    tg_link = f'{Server.BASE_URL}/file/{message_id}?code={secret_code}'
-    deep_link = f'https://t.me/{Telegram.BOT_USERNAME}?start=file_{message_id}_{secret_code}'
-
-   
-
-    if (event.document and 'video' in event.document.mime_type) or event.video:
-        #stream_link = f'{Server.BASE_URL}/stream/{message_id}?code={secret_code}'
-        await event.reply(
-            message= MediaLinksText % {'dl_link': dl_link}, #'tg_link': tg_link, 'tg_link': tg_link, 'stream_link': stream_link},
-            buttons=[
-                [
-                    Button.url('Download', dl_link)
-                   # Button.url('Stream', stream_link)
-                #],
-               # [
-                 #   Button.url('Get File', deep_link),
-                 #   Button.inline('Revoke', f'rm_{message_id}_{secret_code}')
-                ]
-            ]
-        )
-    else:
-        await event.reply(
-            message=FileLinksText % {'dl_link': dl_link}, #'tg_link': tg_link},
-            buttons=[
-                [
-                    Button.url('Download', dl_link)
-                #    Button.url('Get File', deep_link)
-               # ],
-              #  [
-                #    Button.inline('Revoke', f'rm_{message_id}_{secret_code}')
-                ]
-            ]
-        )
-
+# Your original channel file handler remains the same
 @TelegramBot.on(NewMessage(incoming=True, func=filter_files, forwards=False))
 @verify_user()
 async def channel_file_handler(event: NewMessage.Event | Message):
@@ -159,13 +117,10 @@ async def channel_file_handler(event: NewMessage.Event | Message):
     tg_link = f"{Server.BASE_URL}/file/{message_id}?code={secret_code}"
 
     if (event.document and "video" in event.document.mime_type) or event.video:
-        #stream_link = f"{Server.BASE_URL}/stream/{message_id}?code={secret_code}"
-
         try:
             await event.edit(
                 buttons=[
-                    [Button.url("Download", dl_link)]#Button.url("Stream", stream_link)],
-                    #[Button.url("Get File", tg_link)],
+                    [Button.url("Download", dl_link)]
                 ]
             )
         except (
@@ -178,7 +133,7 @@ async def channel_file_handler(event: NewMessage.Event | Message):
         try:
             await event.edit(
                 buttons=[
-                    [Button.url("Download", dl_link)]#, Button.url("Get File", tg_link)]
+                    [Button.url("Download", dl_link)]
                 ]
             )
         except (
@@ -187,4 +142,3 @@ async def channel_file_handler(event: NewMessage.Event | Message):
             MessageNotModifiedError,
         ):
             pass
-
